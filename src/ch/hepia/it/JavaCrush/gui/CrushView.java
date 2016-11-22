9dc733f790e6139420671d0210a3465dedcdd44a
Lovino Maxime <maximelovino@gmail.com>
2016-11-22 20:58:31 +0100

    started adding listener and basic swap

--- src/ch/hepia/it/JavaCrush/gui/CrushView.java
+++ src/ch/hepia/it/JavaCrush/gui/CrushView.java
@@ -2,6 +2,8 @@ package ch.hepia.it.JavaCrush.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
 public class CrushView extends JPanel {
@@ -21,6 +23,7 @@ public class CrushView extends JPanel {
 
 	private static final int SIZE = 10;
 	private JButton[] buttons = new JButton[SIZE*SIZE];
+	int clickedFirst = -1;
 	Random rnd;
 
 	public CrushView (Random rnd) {
@@ -30,7 +33,32 @@ public class CrushView extends JPanel {
 		for (int i = 0; i < buttons.length; i++) {
 			buttons[i] = new JButton(new ImageIcon(new ImageIcon("assets/"+images[rnd.nextInt(images.length)]).getImage().getScaledInstance(60,60,Image.SCALE_DEFAULT)));
 			buttons[i].setName(String.valueOf(i));
+			buttons[i].addActionListener(e -> {
+				JButton source = (JButton) e.getSource();
+				System.out.println(source.getName());
+				if (clickedFirst == -1){
+					clickedFirst = Integer.valueOf(source.getName());
+				}else{
+					System.out.println("it's the second one");
+					int secondClick = Integer.valueOf(source.getName());
+					//TODO check with mods if we're on far left or far right
+					//TODO perhaps do check in swap function
+					if (secondClick == clickedFirst + 1 || secondClick == clickedFirst -1 || secondClick == clickedFirst + SIZE || secondClick == clickedFirst - SIZE){
+						swapTwoButtons(clickedFirst, secondClick);
+					}
+					clickedFirst = -1;
+				}
+			});
 			this.add(buttons[i]);
 		}
+	}
+
+	private void swapTwoButtons(int a, int b){
+		System.out.println("swapping");
+		ImageIcon iconA = (ImageIcon) buttons[a].getIcon();
+		ImageIcon iconB = (ImageIcon) buttons[b].getIcon();
+		buttons[b].setIcon(iconA);
+		buttons[a].setIcon(iconB);
+		this.update(this.getGraphics());
 	}
 }
